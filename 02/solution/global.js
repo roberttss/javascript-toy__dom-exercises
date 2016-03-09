6bdde198d31ae8aa1c620006068408f0f0c3e4c1
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-03-09 13:52:07 -0600

    Almost functioning properly. for some reason, getElementById('hide_me') is a null object.

---
+++ 02/solution/global.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+
+  var button = document.getElementById('button');
+
+  var hide_me = document.getElementById('hide_me');
+
+  button.addEventListener("click", function(hide_me) {
+
+    if (hide_me.style.display=="block")
+      hide_me.style.display=="none"
+    else
+      hide_me.style.display=="block"
+  });
+
+};
+
+
+    
+
+
+
