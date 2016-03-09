ecb3a9faeb16a231cd9f284352adc32d622b1d40
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-03-09 11:31:56 -0600

    cleans, removes code comments

--- 01/solution/global.js
+++ 01/solution/global.js
@@ -1,4 +1,4 @@
-function jSFunction(current, to) {
+function autoTab(current, to) {
   if (current.getAttribute && 
       current.value.length==current.getAttribute("maxlength")) {
         to.focus() 
